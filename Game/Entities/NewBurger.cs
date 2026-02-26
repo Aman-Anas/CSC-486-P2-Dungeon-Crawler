a534bfaac5efe331e8a43b7a0f6b71fec3e6c6fa
Alston Rodriguez <avrodri3@ncsu.edu>
2026-02-26 12:44:25 -0500

    new burger

---
+++ Game/Entities/NewBurger.cs
@@ -0,0 +1,69 @@
+using Godot;
+
+namespace Game.Entities;
+
+public partial class NewBurger : RigidBody3D
+{
+	[Export]
+	Farmer? playerToFollow;
+
+	AnimationPlayer animPlayer = null!;
+
+	[Export]
+	float maximumFollowingDistance = 5.0f;
+
+	[Export]
+	float minimumFollowingDistance = 1.0f;
+
+	[Export]
+	float followSpeed = 5.0f;
+
+	[Export]
+	StringName runAnimation = "BAKED_Walk";
+
+	[Export]
+	StringName idleAnimation = "RESET";
+
+	[Export]
+	float animationBlendAmount = 0.5f;
+
+	public override void _Ready() {
+		animPlayer = GetNode<AnimationPlayer>("Model/AnimationPlayer");
+	}
+	
+	public void Kill() {
+		this.QueueFree();
+	}
+
+	public override void _IntegrateForces(PhysicsDirectBodyState3D state)
+	{
+		if (playerToFollow == null)
+		{
+			return;
+		}
+
+		var localLinearVelocity = GlobalBasis.Inverse() * state.LinearVelocity;
+		localLinearVelocity.X = 0;
+
+		var myPos = GlobalPosition;
+		var playerPos = playerToFollow.GlobalPosition with { Y = myPos.Y };
+
+		this.LookAt(playerPos, Vector3.Up);
+
+		var distanceToPlayer = myPos.DistanceTo(playerPos);
+
+		// If close enough or too far away, do nothing
+		if (distanceToPlayer <= minimumFollowingDistance || distanceToPlayer >= maximumFollowingDistance)
+		{
+			localLinearVelocity.Z = 0;
+			animPlayer.Play(idleAnimation, customBlend: animationBlendAmount);
+		}
+		else
+		{
+			localLinearVelocity.Z = -followSpeed;
+			animPlayer.Play(runAnimation, customBlend: animationBlendAmount);
+		}
+
+		state.LinearVelocity = GlobalBasis * localLinearVelocity;
+	}
+}
