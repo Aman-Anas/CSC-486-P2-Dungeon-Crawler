a2d97be52567fc95f17ff7d5373b16bb689fabf8
Aman-Anas <IA.AmanAnas@gmail.com>
2026-02-26 14:46:42 -0500

    Update editor settings

--- Game/Entities/NewBurger.cs
+++ Game/Entities/NewBurger.cs
@@ -4,66 +4,71 @@ namespace Game.Entities;
 
 public partial class NewBurger : RigidBody3D
 {
-	[Export]
-	Farmer? playerToFollow;
+    [Export]
+    Farmer? playerToFollow;
 
-	AnimationPlayer animPlayer = null!;
+    AnimationPlayer animPlayer = null!;
 
-	[Export]
-	float maximumFollowingDistance = 5.0f;
+    [Export]
+    float maximumFollowingDistance = 5.0f;
 
-	[Export]
-	float minimumFollowingDistance = 1.0f;
+    [Export]
+    float minimumFollowingDistance = 1.0f;
 
-	[Export]
-	float followSpeed = 5.0f;
+    [Export]
+    float followSpeed = 5.0f;
 
-	[Export]
-	StringName runAnimation = "BAKED_Walk";
+    [Export]
+    StringName runAnimation = "BAKED_Walk";
 
-	[Export]
-	StringName idleAnimation = "RESET";
+    [Export]
+    StringName idleAnimation = "RESET";
 
-	[Export]
-	float animationBlendAmount = 0.5f;
+    [Export]
+    float animationBlendAmount = 0.5f;
 
-	public override void _Ready() {
-		animPlayer = GetNode<AnimationPlayer>("Model/AnimationPlayer");
-	}
-	
-	public void Kill() {
-		this.QueueFree();
-	}
+    public override void _Ready()
+    {
+        animPlayer = GetNode<AnimationPlayer>("Model/AnimationPlayer");
+    }
 
-	public override void _IntegrateForces(PhysicsDirectBodyState3D state)
-	{
-		if (playerToFollow == null)
-		{
-			return;
-		}
+    public void Kill()
+    {
+        this.QueueFree();
+    }
 
-		var localLinearVelocity = GlobalBasis.Inverse() * state.LinearVelocity;
-		localLinearVelocity.X = 0;
+    public override void _IntegrateForces(PhysicsDirectBodyState3D state)
+    {
+        if (playerToFollow == null)
+        {
+            return;
+        }
 
-		var myPos = GlobalPosition;
-		var playerPos = playerToFollow.GlobalPosition with { Y = myPos.Y };
+        var localLinearVelocity = GlobalBasis.Inverse() * state.LinearVelocity;
+        localLinearVelocity.X = 0;
 
-		this.LookAt(playerPos, Vector3.Up);
+        var myPos = GlobalPosition;
+        var playerPos = playerToFollow.GlobalPosition with { Y = myPos.Y };
 
-		var distanceToPlayer = myPos.DistanceTo(playerPos);
+        this.LookAt(playerPos, Vector3.Up);
 
-		// If close enough or too far away, do nothing
-		if (distanceToPlayer <= minimumFollowingDistance || distanceToPlayer >= maximumFollowingDistance)
-		{
-			localLinearVelocity.Z = 0;
-			animPlayer.Play(idleAnimation, customBlend: animationBlendAmount);
-		}
-		else
-		{
-			localLinearVelocity.Z = -followSpeed;
-			animPlayer.Play(runAnimation, customBlend: animationBlendAmount);
-		}
+        var distanceToPlayer = myPos.DistanceTo(playerPos);
 
-		state.LinearVelocity = GlobalBasis * localLinearVelocity;
-	}
+        // If close enough or too far away, do nothing
+        if (
+            distanceToPlayer <= minimumFollowingDistance
+            || distanceToPlayer >= maximumFollowingDistance
+        )
+        {
+            localLinearVelocity.Z = 0;
+            animPlayer.Play(idleAnimation, customBlend: animationBlendAmount);
+        }
+        else
+        {
+            localLinearVelocity.Z = -followSpeed;
+            animPlayer.Play(runAnimation, customBlend: animationBlendAmount);
+        }
+
+        state.LinearVelocity = GlobalBasis * localLinearVelocity;
+    }
 }
