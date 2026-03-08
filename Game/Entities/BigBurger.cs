5d314e8522910b631a68da5afbb611a4a955db49
Alston Rodriguez <avrodri3@ncsu.edu>
2026-03-08 16:36:56 -0400

    burger boss script

---
+++ Game/Entities/BigBurger.cs
@@ -0,0 +1,11 @@
+using System;
+using Game.UI;
+using Godot;
+using GodotTask;
+
+namespace Game.Entities;
+
+public partial class BigBurger : BurgerEnemy
+{
+    public float animationPlaybackSpeedMod = 1.5f / 3.5f;
+}
