5ec725e6bc619fbddee6e6e7999ddd071fe71735
Aman-Anas <IA.AmanAnas@gmail.com>
2026-03-09 00:07:31 -0400

    Add some dialogue triggers to Level 0

--- Game/Entities/BobTrigger.cs
+++ Game/Entities/BobTrigger.cs
@@ -6,7 +6,7 @@ namespace Game.Entities;
 
 public partial class BobTrigger : Area3D
 {
-    [Export]
+    [Export(PropertyHint.MultilineText)]
     public string Dialogue { get; set; } = "";
 
     // Called when the node enters the scene tree for the first time.
@@ -17,9 +17,21 @@ public partial class BobTrigger : Area3D
 
     private void DetectBob(Node3D body)
     {
-        if (body is NewBob Bob)
+        // How long to take to reveal each char (in ms)
+        const int CharRevealTime = 10; // ms
+
+        if (body is NewBob Bob && (Bob.SpeechLabel.Text != Dialogue))
         {
             Bob.SpeechLabel.Text = Dialogue;
+            var typewriteTime = CharRevealTime * Dialogue.Length;
+
+            var newTween = GetTree().CreateTween();
+            newTween.TweenMethod(
+                Callable.From((float amt) => Bob.SpeechLabel.VisibleRatio = amt),
+                0f,
+                1f,
+                typewriteTime / 1000f
+            );
         }
     }
 
